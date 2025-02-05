08af98157f16fcfb03d956290f582cd63ff8934f
Kayan Laswell <kcclaswell@gmail.com>
2025-02-05 17:51:52 -0500

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>Change the numbers in checkout</h3>
+    <div>
+      Eggs
+      <button @click="increment">+</button>
+
+      <button @click="decrement">-</button>
+      :
+      {{ count }}
+    </div>
+    <div>
+      Bread
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      :
+      {{ count }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      counters: {
+        counter1: 0,
+        counter2: 0,
+        counter3: 0,
+      },
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      if (this.count > 0) {
+        this.count--
+      }
+    },
+  },
+}
+</script>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
