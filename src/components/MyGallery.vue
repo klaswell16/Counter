c1366b5657851eda9dfe4e25dca22efa59a7b714
Kayan Laswell <kcclaswell@gmail.com>
2025-02-11 12:04:13 -0500

    Add pictures to gallery component

--- src/components/MyGallery.vue
+++ src/components/MyGallery.vue
@@ -8,6 +8,20 @@ defineProps({
 </script>
 <template>
   <div>
-    <img src=""
+    <img
+      src="C:/Users/kccla/Vue_Projects/Counter/src/assets/mountain.jpg"
+      alt="Mountain"
+      style="width: 200px; height: 200px"
+    />
+    <img
+      src="C:/Users/kccla/Vue_Projects/Counter/src/assets/truck.jpg"
+      alt="Truck"
+      style="width: 200px; height: 200px"
+    />
+    <img
+      src="C:/Users/kccla/Vue_Projects/Counter/src/assets/city.jpg"
+      alt="City"
+      style="width: 200px; height: 200px"
+    />
   </div>
 </template>
